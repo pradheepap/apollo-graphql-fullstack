8e29e4240ac339aad8d62c0e26f764b66833b7f1
pradheepa <pradheepa.p@gmail.com>
2020-12-30 13:08:08 +0800

    Add query resolvers

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
 
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
@@ -10,17 +11,16 @@ const UserAPI = require('./datasources/user');
 const store = createStore();
 
 
-const server = new ApolloServer({ typeDefs,
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
     dataSources: () => ({
         launchAPI: new LaunchAPI(),
         userAPI: new UserAPI({ store })
-      }) });
+    })
+});
 
-server.listen().then(() => {
-    console.log(`
-      Server is running!
-      Listening on port 4000
-      Explore at https://studio.apollographql.com/dev
-    `);
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
   });
-  
