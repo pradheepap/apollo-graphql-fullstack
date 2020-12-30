c4a092881716c2b099fca96d66c0704b50b1e720
pradheepa <pradheepa.p@gmail.com>
2020-12-30 13:31:16 +0800

    Add Pagination

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -10,45 +10,65 @@ const typeDefs = gql`
     }
 
     type Rocket {
-  id: ID!
-  name: String
-  type: String
-}
+        id: ID!
+        name: String
+        type: String
+    }
 
-type User {
-  id: ID!
-  email: String!
-  trips: [Launch]!
-  token: String
-}
+    type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+    token: String
+    }
 
-type Mission {
-  name: String
-  missionPatch(size: PatchSize): String
-}
+    type Mission {
+        name: String
+        missionPatch(size: PatchSize): String
+    }
 
-enum PatchSize {
-  SMALL
-  LARGE
-}
+    enum PatchSize {
+        SMALL
+        LARGE
+    }
 
-type Query {
+    type Query {
+        launches( # replace the current launches query with this one.
+            """
+            The number of results to show. Must be >= 1. Default = 20
+            """
+            pageSize: Int
+            """
+            If you add a cursor here, it will only return results _after_ this cursor
+            """
+            after: String
+        ): LaunchConnection!
+        launch(id: ID!): Launch
+        me: User
+    }
+
+    """
+Simple wrapper around our list of launches that contains a cursor to the
+last item in the list. Pass this cursor to the launches query to fetch results
+after these.
+"""
+type LaunchConnection { # add this below the Query type as an additional type.
+  cursor: String!
+  hasMore: Boolean!
   launches: [Launch]!
-  launch(id: ID!): Launch
-  me: User
 }
 
-type Mutation {
-  bookTrips(launchIds: [ID]!): TripUpdateResponse!
-  cancelTrip(launchId: ID!): TripUpdateResponse!
-  login(email: String): User
-}
+    type Mutation {
+        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        cancelTrip(launchId: ID!): TripUpdateResponse!
+        login(email: String): User
+    }
 
-type TripUpdateResponse {
-  success: Boolean!
-  message: String
-  launches: [Launch]
-}
+    type TripUpdateResponse {
+        success: Boolean!
+        message: String
+        launches: [Launch]
+    }
 
 
 `;
